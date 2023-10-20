976626cb40450dda514fd5eeb5c692ea30ef60ec
Tommy Cheuk Ho Wong <146346053+chw23@users.noreply.github.com>
2023-10-20 16:59:12 +0000

    week3 lecture2

---
+++ week3_c_bootcamp/lecture2/worksheet/bronze_level.c
@@ -0,0 +1,26 @@
+# include <stdio.h>
+# include <math.h>
+
+/*
+// question 1
+float circleArea (float r) {
+    float area;
+    area = M_PI * r * r;
+    return area;
+}
+
+int main () {
+    float radius;
+
+    printf("Enter a value for radius: ");
+    scanf("%f", &radius);
+
+    float area = circleArea(radius);
+
+    printf("The area of the circle is %.2f.\n", area);
+
+    return 0;
+}
+*/
+
+// question 2
