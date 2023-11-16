23abc7d82ad4119d1013eaa7a0fcd4f36a4b0877
Tommy Cheuk Ho Wong <146346053+chw23@users.noreply.github.com>
2023-11-16 20:14:19 +0000

    coursework part 2 update

--- coursework/part2/FitnessDataStruct.h
+++ coursework/part2/FitnessDataStruct.h
@@ -1,8 +1,12 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 #ifndef FITNESS_DATA_STRUCT_H
 #define FITNESS_DATA_STRUCT_H
 
 #include <stdio.h>
+#define buffer_size 100
 
 // Define an appropriate struct
 typedef struct {
@@ -12,7 +16,30 @@ typedef struct {
 } FITNESS_DATA;
 
 // Helper function prototypes
-void tokeniseRecord(const char *input, const char *delimiter, char *date, char *time, char *steps);
+void tokeniseRecord(const char *input, const char *delimiter,
+                    char *date, char *time, char *steps) {
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+    
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    if (token != NULL) {        strcpy(date, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(time, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(steps, token);
+    }
+    
+    // Free the duplicated string
+    free(inputCopy);
+
+                    }
 
 #endif // FITNESS_DATA_STRUCT_H
   
--- coursework/part2/StepCounter_Final.c
+++ coursework/part2/StepCounter_Final.c
@@ -12,6 +12,7 @@
 // This is your helper function. Do not change it in any way.
 // Inputs: character array representing a row; the delimiter character
 // Ouputs: date character array; time character array; steps character array
+/*
 void tokeniseRecord(const char *input, const char *delimiter,
                     char *date, char *time, char *steps) {
     // Create a copy of the input string as strtok modifies the string
@@ -36,13 +37,82 @@ void tokeniseRecord(const char *input, const char *delimiter,
     free(inputCopy);
 
                     }
+*/
 
 
-
-
+// Inspired by week7 material: code.c 
 // Complete the main function
 int main() {
+   char buffer[buffer_size];
+
+   char choice;
+   int counter = 0;
    
+   while (1) {
+    FILE *input;
+    input = fopen("FitnessData_2023.csv", "r");
+
+    printf("A: Specify the filename to be imported\n");
+    printf("B: Display the total number of records in the file\n");
+    printf("C: Find the date and time of the timeslot with the fewest steps\n");
+    printf("D: Find the data and time of the timeslot with the largest number of steps\n");
+    printf("E: Find the mean step count of all the records in the file\n");
+    printf("F: Find the longest continuous period where the step count is above 500 steps\n");
+    printf("Q: Exit the program\n");
+
+    choice = getchar();
+
+    while (getchar() != '\n');
+
+    switch (choice) {
+        case 'A':
+        case 'a':
+            printf("Input filename: %s\n", input);
+            if (!input) {
+                printf("Error: Could not find or open the file.\n");
+            }
+            else {
+                printf("File successfully loaded.\n");
+            }
+            break;
+
+        case 'B':
+        case 'b':
+            return 0;
+            break;
+
+        case 'C':
+        case 'c':
+            return 0;
+            break;
+
+        case 'D':
+        case 'd':
+            return 0;
+            break;
+
+        case 'E':
+        case 'e':
+            return 0;
+            break;
+
+        case 'F':
+        case 'f':
+            return 0;
+            break;
+
+        case 'Q':
+        case 'q':
+            return 0;
+            break;
+        
+        default:
+            printf("Invalid choice\n");
+            break;
+    }
+   }
+
+
 }
 
 
